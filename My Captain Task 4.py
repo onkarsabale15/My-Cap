9d53cfec0ef9ac05a3a3e818b58092dde1786c90
onkarsabale15 <80263717+onkarsabale15@users.noreply.github.com>
2021-03-19 20:15:44 +0530

    Add files via upload

---
+++ My Captain Task 4.py
@@ -0,0 +1,9 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+print("Output:",end="")
+for num in list1:
+    if num>=0:
+        print(num,end=",")
+for no in list2:
+    if no>=0:
+        print(no,end=",")
