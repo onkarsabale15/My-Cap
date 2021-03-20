409397a95337a25f2e814b1dac05d0349b4ed772
onkarsabale15 <80263717+onkarsabale15@users.noreply.github.com>
2021-03-20 13:34:29 +0530

    Update My Captain Task 4.py

--- My Captain Task 4.py
+++ My Captain Task 4.py
@@ -1,9 +1,12 @@
+output_list1=[]
 list1 = [12, -7, 5, 64, -14]
-list2 = [12, 14, -95, 3]
-print("Output:",end="")
 for num in list1:
     if num>=0:
-        print(num,end=",")
+        output_list1.append(num)
+print(output_list1)
+output_list2=[]
+list2 = [12, 14, -95, 3]
 for no in list2:
     if no>=0:
-        print(no,end=",")
+        output_list2.append(no)
+print(output_list2)
